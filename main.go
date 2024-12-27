fa5c7739cd503b7bf2b3f4e90a19160c6bb47376
A. Samuel <aaron.psamuel@gmail.com>
2024-12-26 22:15:48 -0500

    chore: use flags

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+
+	"github.com/apsamuel/brainiac/pkg/common"
 )
 
+var ConfigFile string
+
+func init() {
+	flag.StringVar(&ConfigFile, "config", "./config/brainiac.yaml", "Path to the configuration file")
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(common.Foo)
 }
