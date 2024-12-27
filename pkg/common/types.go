38f9cdcc73f26f87a624fda6a224a7e16fb722c4
A. Samuel <aaron.psamuel@gmail.com>
2024-12-26 22:12:23 -0500

    chore: supporting database structs

---
+++ pkg/common/types.go
@@ -0,0 +1,21 @@
+package common
+
+import (
+	"context"
+)
+
+type Storer[T any] interface {
+	Get(key string) (T, error)
+	Set(key string, value T) error
+	Retrieve(query string) ([]T, error)
+	RetrieveById(id string) (T, error)
+	VectorSearch(queryVector []float64) ([]T, error)
+	Execute(ctx context.Context, query string, args ...interface{}) ([]interface{}, error)
+}
+
+type Schema interface {
+	String() string
+	GetId() string
+	TableName() string
+	Columns() []string
+}
