a422831af92b10cae048f5e0314fef5c19281a0c
A. Samuel <aaron.psamuel@gmail.com>
2024-12-29 13:18:22 -0500

    chore: configures database

--- main.go
+++ main.go
@@ -4,16 +4,35 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/apsamuel/brainiac/pkg/common"
+	"github.com/apsamuel/brainiac/pkg/database"
 )
 
 var ConfigFile string
+var Debug bool
 
 func init() {
 	flag.StringVar(&ConfigFile, "config", "./config/brainiac.yaml", "Path to the configuration file")
+	flag.BoolVar(&Debug, "debug", false, "Enable debug mode")
 	flag.Parse()
 }
 
+/*
+ * This is the main entry point for the brainiac application.
+ */
 func main() {
-	fmt.Println(common.Foo)
+
+	var databaseConfig database.Config
+	if Debug {
+		fmt.Println("debug mode enabled")
+	}
+	err := databaseConfig.Configure(ConfigFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	storage, err := database.MakeStorage(databaseConfig)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(storage)
+
 }
