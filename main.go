ae88201710929062231c920d1178bc3e1c3da95d
A. Samuel <aaron.psamuel@gmail.com>
2024-12-31 11:02:33 -0500

    chore: use zerolog

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/apsamuel/brainiac/pkg/cache"
 	"github.com/apsamuel/brainiac/pkg/database"
+	"github.com/apsamuel/brainiac/pkg/logger"
 )
 
 var ConfigFile string
@@ -22,28 +22,34 @@ func init() {
  */
 func main() {
 
+	l := logger.Logger
+	l.Logger.Info().Msg("starting brainiac")
+
 	var databaseConfig database.Config
 	var cacheConfig cache.Config
 	if Debug {
-		fmt.Println("debug mode enabled")
+		l.Logger.Debug().Msg("debugging enabled")
 	}
 
 	err := databaseConfig.Configure(ConfigFile)
 	if err != nil {
-		fmt.Println(err)
+		l.Logger.Error().Msg(err.Error())
 	}
+	databaseConfig.Log = &l.Logger
 
 	_, err = database.MakeStorage(databaseConfig)
 	if err != nil {
-		fmt.Println(err)
+		l.Logger.Error().Msg(err.Error())
 	}
 
 	err = cacheConfig.Configure(ConfigFile)
 	if err != nil {
-		fmt.Println(err)
+		l.Logger.Error().Msg(err.Error())
 	}
+
+	cacheConfig.Log = &l.Logger
 	_, err = cache.MakeStorage(cacheConfig)
 	if err != nil {
-		fmt.Println(err)
+		l.Logger.Error().Msg(err.Error())
 	}
 }
