0eb9b9ce0f408f5a0ffdd9f5403fb796c40049b5
A. Samuel <aaron.psamuel@gmail.com>
2025-02-17 11:24:50 -0500

    chore: updates to main, tests configuration refactor

--- main.go
+++ main.go
@@ -4,16 +4,18 @@ import (
 	"flag"
 
 	"github.com/apsamuel/brainiac/pkg/cache"
+	"github.com/apsamuel/brainiac/pkg/common"
 	"github.com/apsamuel/brainiac/pkg/database"
+	"github.com/apsamuel/brainiac/pkg/extensions/api"
 	"github.com/apsamuel/brainiac/pkg/logger"
 )
 
-var ConfigFile string
-var Debug bool
+var configFile string
+var debug bool
 
 func init() {
-	flag.StringVar(&ConfigFile, "config", "./config/brainiac.yaml", "Path to the configuration file")
-	flag.BoolVar(&Debug, "debug", false, "Enable debug mode")
+	flag.StringVar(&configFile, "config", "./config/brainiac.yaml", "Path to the configuration file")
+	flag.BoolVar(&debug, "debug", false, "Enable debug mode")
 	flag.Parse()
 }
 
@@ -21,35 +23,100 @@ func init() {
  * This is the main entry point for the brainiac application.
  */
 func main() {
+	var databaseConfig database.Config
+	var cacheConfig cache.Config
+	var apiConfig api.Config
 
+	var brainiacConfig common.Config
+
+	// debug brainiacConfig
+
+	observerChannels := make(map[string]chan common.Item)
+	for _, observer := range []string{"api", "proxy"} {
+		observerChannels[observer] = make(chan common.Item)
+	}
 	l := logger.Logger
 	l.Logger.Info().Msg("starting brainiac")
 
-	var databaseConfig database.Config
-	var cacheConfig cache.Config
-	if Debug {
+	if debug {
 		l.Logger.Debug().Msg("debugging enabled")
 	}
 
-	err := databaseConfig.Configure(ConfigFile)
+	_, err := brainiacConfig.FromFile(configFile)
+	if err != nil {
+		logger.Logger.Logger.Error().Msg(err.Error())
+		return
+	}
+	l.Logger.Info().Interface("configuration", brainiacConfig).Msg("loaded brainiac configuration")
+	jsonConfig, err := brainiacConfig.ToInterface()
+	if err != nil {
+		l.Logger.Error().Msg(err.Error())
+		return
+	}
+	l.Logger.Info().Interface("json configuration", jsonConfig).Msg("loaded brainiac configuration")
+
+	err = databaseConfig.Configure(configFile)
 	if err != nil {
 		l.Logger.Error().Msg(err.Error())
 	}
 	databaseConfig.Log = &l.Logger
 
-	_, err = database.MakeStorage(databaseConfig)
+	storage, err := database.MakeStorage(databaseConfig)
 	if err != nil {
 		l.Logger.Error().Msg(err.Error())
 	}
 
-	err = cacheConfig.Configure(ConfigFile)
+	err = cacheConfig.Configure(configFile)
 	if err != nil {
 		l.Logger.Error().Msg(err.Error())
 	}
 
 	cacheConfig.Log = &l.Logger
-	_, err = cache.MakeStorage(cacheConfig)
+	cacheStorage, err := cache.MakeStorage(cacheConfig)
 	if err != nil {
 		l.Logger.Error().Msg(err.Error())
 	}
+
+	/* configure and start API server */
+
+	err = apiConfig.Configure(configFile)
+	if err != nil {
+		l.Logger.Error().Msg(err.Error())
+	}
+
+	apiConfig.Log = &l.Logger
+
+	apiHandler := api.Handler{
+		Config:    &apiConfig,
+		Observers: observerChannels,
+		Storage:   storage,
+		Cache:     &cacheStorage,
+	}
+	err = apiHandler.MakeRouter()
+	if err != nil {
+		l.Logger.Error().Msg(err.Error())
+	}
+
+	for _, agentRoutes := range [][]*common.Route{
+		apiHandler.ListRoutes(),
+	} {
+		for _, route := range agentRoutes {
+			err := apiHandler.AddRoute(route)
+			if err != nil {
+				l.Logger.Error().Msg(err.Error())
+				return
+			}
+		}
+	}
+
+	go func() {
+		l.Logger.Info().Msg("starting brainiac api server")
+		err := apiHandler.Serve()
+		if err != nil {
+			l.Logger.Error().Msg(err.Error())
+			return
+		}
+	}()
+
+	select {}
 }
