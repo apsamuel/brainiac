cca979ba891651cab3e7c7f4f5b2497f4dcf48a3
A. Samuel <aaron.psamuel@gmail.com>
2025-02-17 11:20:00 -0500

    chore: use AddRoute to register server routes

--- pkg/extensions/api/handler.go
+++ pkg/extensions/api/handler.go
@@ -3,7 +3,6 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"text/template"
 
 	"github.com/apsamuel/brainiac/pkg/cache"
@@ -11,23 +10,74 @@ import (
 	"github.com/apsamuel/brainiac/pkg/database"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
-	"github.com/rs/zerolog"
 )
 
+type MethodHandleFuncs struct {
+	Methods    []string
+	Public     string
+	HandleFunc http.HandlerFunc
+}
 type Handler struct {
-	Config    *Config
-	Router    *mux.Router
-	Storage   *database.Storage
-	Cache     *cache.RedisStorage
-	Log       zerolog.Logger
-	Observers map[string]chan common.Item
-	Templater *template.Template
+	Config                   *Config
+	Router                   *mux.Router
+	EndpointMethodHandlerMap map[string]*common.Route
+	Storage                  *database.Storage
+	Cache                    *cache.RedisStorage
+	Observers                map[string]chan common.Item
+	Templater                *template.Template
+}
+
+func (h *Handler) ConsumeEvents(eventChannel chan common.Item) error {
+	for item := range eventChannel {
+		h.Config.Log.Info().Msgf("Received event: %v", item)
+	}
+	return nil
+}
+
+func (h *Handler) ToEventChannel(item common.Item) {
+	c, ok := h.Observers[item.Destination]
+	if !ok {
+		h.Config.Log.Error().Msg("Observer not found")
+		return
+	}
+
+	c <- item
 }
 
 func (h *Handler) MakeRouter() error {
 	h.Router = mux.NewRouter()
-	h.Log = zerolog.New(os.Stdout).With().Timestamp().Logger()
+	functionMap := template.FuncMap{
+		"foo": func() string {
+			return "bar"
+		},
+	}
+	h.Templater = template.Must(template.New("index").Funcs(functionMap).ParseGlob(fmt.Sprintf("%s/*.html", h.Config.Options.TemplateDir)))
+	// h.EndpointMethodHandlerMap = make(map[string]MethodHandleFuncs)
+	// _ = h.AddRoute("/health", []string{"GET"}, h.Health, "public")
 	return nil
+}
+
+func (h *Handler) AddRoute(route *common.Route) error {
+
+	if route == nil {
+		return fmt.Errorf("route is nil")
+	}
+
+	h.Router.HandleFunc(route.Endpoint, route.Handler).Methods(route.Methods...)
+
+	return nil
+}
+
+func (h *Handler) ListRoutes() []*common.Route {
+	routes := []*common.Route{
+		{
+			Endpoint: "/health",
+			Methods:  []string{"GET"},
+			Handler:  h.Health,
+			Auth:     "public",
+		},
+	}
+	return routes
 }
 
 func (h *Handler) Serve() error {
@@ -42,14 +92,15 @@ func (h *Handler) Serve() error {
 		AllowPrivateNetwork:  true,
 		OptionsPassthrough:   false,
 		OptionsSuccessStatus: 204,
-		Debug:                false,
-		// Logger:               logger.Logger,
+		Debug:                h.Config.Options.Debug,
+		Logger:               h.Config.Log,
 	}
 	corsMiddleware := cors.New(corsOptions).Handler(h.Router)
 
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", h.Config.Options.Host, h.Config.Options.Port), corsMiddleware); err != nil {
-		h.Log.Error().Err(err).Msg("Failed to start server")
+		h.Config.Log.Error().Err(err).Msg("failed to start server")
 		return err
 	}
+	h.Config.Log.Info().Msg("server started")
 	return nil
 }
