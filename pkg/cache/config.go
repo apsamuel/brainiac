350ded5614b7993f265aae6dd1f3dc039c5f6720
A. Samuel <aaron.psamuel@gmail.com>
2025-03-18 17:59:00 -0400

    chore: several fixes and updates for the cache module

--- pkg/cache/config.go
+++ pkg/cache/config.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"encoding/json"
 	"os"
 
 	"github.com/rs/zerolog"
@@ -8,13 +9,13 @@ import (
 )
 
 type Config struct {
-	Options Options `yaml:"cache"`
-	Log     *zerolog.Logger
+	Options Options         `yaml:"cache" json:"cache"`
+	Log     *zerolog.Logger `yaml:"-" json:"-"`
 }
 
 type Options struct {
-	Engine string      `yaml:"engine"`
-	Redis  RedisConfig `yaml:"redis"`
+	Engine string      `yaml:"engine" json:"engine"`
+	Redis  RedisConfig `yaml:"redis" json:"redis"`
 }
 
 func (c *Config) ConfigureFromFile(filename string) error {
@@ -27,4 +28,45 @@ func (c *Config) ConfigureFromFile(filename string) error {
 		return err
 	}
 	return nil
+}
+
+/*
+ConfigureFromInterface configures a Config struct from an interface `map[string]any`
+*/
+func (c *Config) ConfigureFromInterface(data map[string]interface{}) error {
+
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(dataBytes, c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/*
+String converts a Config struct to a JSON encoded string
+*/
+func (c *Config) String() string {
+	// returns JSON encoded string
+	data, err := yaml.Marshal(c)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
+
+/*
+ToInterface converts a Config struct to an interface
+*/
+func (c *Config) ToInterface() map[string]interface{} {
+	interfaceData := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(c.String()), &interfaceData)
+	if err != nil {
+		return nil
+	}
+	return interfaceData
 }
--- pkg/cache/types.go
+++ pkg/cache/types.go
@@ -3,8 +3,8 @@ package cache
 import "github.com/redis/go-redis/v9"
 
 type RedisConfig struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
+	Host     string `yaml:"host" json:"host"`
+	Port     int    `yaml:"port" json:"port"`
 	Password string `yaml:"password" json:"-"`
 	Database int    `yaml:"database"`
 }
